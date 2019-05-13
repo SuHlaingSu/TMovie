7b4f1dde2fde864f83fcb4d0e7d6c4cfeda9444d
su hlaing <dev.suhlaing@gmail.com>
2019-05-13 10:04:10 +0700

    Add live data and listen response

--- app/src/main/java/com/sh/tmovie/view/MainActivity.java
+++ app/src/main/java/com/sh/tmovie/view/MainActivity.java
@@ -1,5 +1,8 @@
 package com.sh.tmovie.view;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -11,6 +14,7 @@ import com.sh.tmovie.R;
 import com.sh.tmovie.adapter.MovieAdapter;
 import com.sh.tmovie.model.entity.Movies;
 import com.sh.tmovie.utilis.Constants;
+import com.sh.tmovie.viewModel.MainActivityViewModel;
 import com.sh.tmovie.webServices.ApiInterface;
 import com.sh.tmovie.webServices.ServiceGenerator;
 
@@ -28,42 +32,28 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView rvMoviesListView;
     private MovieAdapter mMovieAdapter;
     private ArrayList<Movies> moviesList = new ArrayList<>();
+    MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        viewModel.getMoviesList();
         rvMoviesListView = findViewById(R.id.rv_gridView);
+
         rvMoviesListView.setLayoutManager(new GridLayoutManager(this, 2));
 
-        mMovieAdapter=new MovieAdapter(getApplicationContext(), moviesList);
-        rvMoviesListView.setAdapter(mMovieAdapter);
-        getMoviesList();
-    }
+        mMovieAdapter = new MovieAdapter(getApplicationContext(), moviesList);
 
-    private void getMoviesList() {
-        ApiInterface apiInterface = ServiceGenerator.createService(ApiInterface.class);
-        Call<MoviesResponse> call = apiInterface.getMoviesList(Constants.API_KEY);
-        call.enqueue(new Callback<MoviesResponse>() {
+        viewModel.getLive_movies().observe(this, new Observer<List<Movies>>() {
             @Override
-            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
-                if(response.isSuccessful()) {
-                    Log.d(TAG, "Loading successfully, size: " + response.body());
-                    for(Movies movies: response.body().getResults()){
-                        moviesList.add(movies);
-                    }
-                    mMovieAdapter.notifyDataSetChanged();
-                }else {
-                    Log.d(TAG, "Fail" + response.message());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<MoviesResponse> call, Throwable t) {
-                Log.d(TAG, "Fail: " + t.getMessage());
+            public void onChanged(List<Movies> movies) {
+               moviesList.addAll(movies);
+               mMovieAdapter.notifyDataSetChanged();
 
             }
-
         });
+        rvMoviesListView.setAdapter(mMovieAdapter);
     }
 }
