b444c9b21a031480cb563b4cfd0f3ac92545ed31
SuHlaingSu <50191488+SuHlaingSu@users.noreply.github.com>
2019-05-27 18:38:00 +0700

    add dragger dependencies

---
+++ app/src/main/java/com/sh/tmovie/di/module/RetrofitModule.java
@@ -0,0 +1,33 @@
+package com.sh.tmovie.di.module;
+
+import com.suhlaing.mydragger.constant.Constants;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+public class RetrofitModule {
+
+    @Provides
+    public HttpLoggingInterceptor httpLoggingInterceptor()
+    {
+        return new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    }
+
+    @Provides
+    public OkHttpClient okHttpClient(HttpLoggingInterceptor httpLoggingInterceptor)
+    {
+        return new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build();
+    }
+
+    @Provides
+    public Retrofit retrofit(OkHttpClient okHttpClient)
+    {
+        return new Retrofit.Builder().addCallAdapterFactory(RxJava2CallAdapterFactory.create()).addConverterFactory(GsonConverterFactory.create()).baseUrl(Constants.BASE_URL).client(okHttpClient).build();
+    }
+}
